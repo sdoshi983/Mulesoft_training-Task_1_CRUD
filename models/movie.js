050002b075777bd5e2a6d6ada4cc553e5ca9254d
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-08 19:10:01 +0530

    movie schema created

--- models/movie.js
+++ models/movie.js
@@ -1,1 +1,10 @@
 const mongoose = require('mongoose'); 
+
+// creating book schema for structuring and managing the No-SQL data in our mongodb collection
+const bookSchema = mongoose.Schema({
+    movieName: String,
+    director: String,
+    actor: String,
+    actress: String,
+    yearOfRelease: Number,
+});

0dce46d432fa8c585b2e3ac9b6f24d691abb9f58
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-08 18:54:45 +0530

    database connectivity added

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const databaseURL = 'mongodb://localhost:27017/movies';
+const databaseURL = 'mongodb://localhost:27017/movies'; // database's url to be connected
+
+mongoose.connect(databaseURL, {useNewUrlParser: true}).then(() => {
+    console.log('connected to database successfully .. !');
+})

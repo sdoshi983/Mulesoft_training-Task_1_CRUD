71c66aa75984fd8833e31f80ec2129d12684ed94
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-08 20:51:25 +0530

    fetch by movie name route added

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const req = require('express/lib/request');
 const router = express.Router();
 
 const Movie = require('../models/movie');
@@ -7,6 +8,13 @@ router.get('/movie', async (request, response) => {  // fetch
     const data = await Movie.find();
     response.send(data);
 });
+
+router.get('/movie/:movieName', async (request, response) => {  // fetch by movie
+    const movieName = request.params.movieName;
+    const data = await Movie.findOne({movieName: movieName});
+    response.send(data);
+});
+
 
 router.post('/movie', async (request, response) => {    // insert
     const movie = new Movie(request.body);

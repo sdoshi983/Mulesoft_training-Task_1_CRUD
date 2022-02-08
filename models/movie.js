e51ec667a288fb118ef8e040f182f76c51b5577c
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-08 20:39:05 +0530

    model schema exported

--- models/movie.js
+++ models/movie.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose'); 
 
-// creating book schema for structuring and managing the No-SQL data in our mongodb collection
-const bookSchema = mongoose.Schema({
+// creating movie schema for structuring and managing the No-SQL data in our mongodb collection
+const movieSchema = mongoose.Schema({
     movieName: String,
     director: String,
     actor: String,
     actress: String,
     yearOfRelease: Number,
 });
+
+module.exports = mongoose.model('Detail', materialSchema);

64c8f4a48aa6d62cb88fdfbd38727bd58a5195b6
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-08 19:03:12 +0530

    code of starting server added

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ mongoose.connect(databaseURL, {useNewUrlParser: true}).then(() => {
     app.use(bodyParser.json()); // Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
     app.use(bodyParser.urlencoded({extended: false}));  // Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
 
+    app.listen(port, () => {    // starting server
+        console.log('server started');
+    });
+    
 }).catch((exception) => {      // handling exception generated on database connectivity
     console.log(exception.toString());
 })

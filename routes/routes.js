5f287a0c2c1211d752f272259293142447ae25a0
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-08 20:40:55 +0530

    fetch route added

--- routes/routes.js
+++ routes/routes.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
+const Movie = require('../models/movie');
+
+router.get('/movie', async (request, response) => {  // fetch
+    const data = await Movie.find();
+    response.send(data);
+});
